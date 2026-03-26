9dd90f4764230c626448745b0d1fb9fe10f4e7a5
Belova Vera <belova2568@gmail.com>
2026-03-26 19:16:28 +0300

    Завершение функции searchWikipedia

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -21,8 +21,9 @@ void searchWikipedia(List<String>? arguments) {
   } else {
     articleTitle = arguments.join(' ');
   }
-
-  print('Current article title: $articleTitle');
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
 }
 void printUsage() { 
   print(
