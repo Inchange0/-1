2bd32e8719bf92d0ed58a3902edf705d165460f6
Belova Vera <belova2568@gmail.com>
2026-03-26 19:10:41 +0300

    Вызов функции searchWikipedia из main

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -5,7 +5,9 @@ void main(List<String> arguments) {
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
   } else if (arguments.first == 'search') {
-    print('Search command recognized!');
+    // Add this new block:
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
   } else {
     printUsage();
   }
