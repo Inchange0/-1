d273f918d3cba63f5d7dc9beda0f2bbcac42086b
Belova Vera <belova2568@gmail.com>
2026-03-26 19:34:31 +0300

    Создание URL API внутри функции getWikipediaArticle

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+import 'package:http/http.dart' as http; 
+
 const version = '0.0.1';
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
@@ -6,7 +8,6 @@ void main(List<String> arguments) {
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
   } else if (arguments.first == 'search') {
-    // Add this new block:
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
     searchWikipedia(inputArgs);
   } else {
@@ -29,4 +30,11 @@ void printUsage() {
   print(
     "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
   );
+}
+Future<String> getWikipediaArticle(String articleTitle) async {
+   final url = Uri.https(
+    'en.wikipedia.org', 
+    '/api/rest_v1/page/summary/$articleTitle', 
+  );
+ 
 }
