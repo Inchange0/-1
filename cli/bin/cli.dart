b155265f38f87b7ac2d3934fecbdf8fcf32d251f
Belova Vera <belova2568@gmail.com>
2026-03-26 19:07:59 +0300

    Интеграция команды search в  main

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,13 +1,16 @@
 const version = '0.0.1';
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage(); 
+    printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    print('Search command recognized!');
   } else {
-    printUsage(); 
+    printUsage();
   }
 }
+
 void printUsage() { 
   print(
     "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
