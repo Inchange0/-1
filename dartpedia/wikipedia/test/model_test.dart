755f86e7f8251d8e7db6f416319266434da4bfa0
Belova Vera <belova2568@gmail.com>
2026-04-01 21:09:47 +0300

    Добавлен тест десериализации JSON-файла со статьёй о Dart в объект Summary с проверкой канонического заголовка

--- dartpedia/wikipedia/test/model_test.dart
+++ dartpedia/wikipedia/test/model_test.dart
@@ -1,5 +1,13 @@
 void main() {
   group('deserialize example JSON responses from wikipedia API', () {
-    
+    test('deserialize Dart Programming Language page summary example data from '
+        'json file into a Summary object', () async {
+      final String pageSummaryInput =
+          await File(dartLangSummaryJson).readAsString();
+      final Map<String, Object?> pageSummaryMap =
+          jsonDecode(pageSummaryInput) as Map<String, Object?>;
+      final Summary summary = Summary.fromJson(pageSummaryMap);
+      expect(summary.titles.canonical, 'Dart_(programming_language)');
+    });
   });
 }
