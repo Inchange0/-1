60badc577590d0ae647a98ca9edd430e354fe415
Belova Vera <belova2568@gmail.com>
2026-04-01 21:55:04 +0300

    Добавлена полная реализация initFileLogger с настройкой уровня логирования и записью сообщений в файл

--- dartpedia/cli/lib/src/logger.dart
+++ dartpedia/cli/lib/src/logger.dart
@@ -9,10 +9,17 @@ Logger initFileLogger(String name) {
   final projectDir = scriptFile.parent.parent.path;
   final dir = Directory('$projectDir/logs');
   if (!dir.existsSync()) dir.createSync();
-  
   final logFile = File(
     '${dir.path}/${now.year}_${now.month}_${now.day}_$name.txt',
   );
+  logger.level = Level.ALL;
+  logger.onRecord.listen((record) {
+    final msg =
+        '[${record.time} - ${record.loggerName}] ${record.level.name}: ${record.message}';
+    logFile.writeAsStringSync('$msg \n', mode: FileMode.append);
+  });
+
   return logger;
 }
+
 
