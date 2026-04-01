c84969f95525595a5c5d7b9edbfe7f33536f2535
Belova Vera <belova2568@gmail.com>
2026-04-01 19:54:23 +0300

    экспортирует файл src/console.dart, делая утилиты для цветного вывода в консоль доступными для других пакетов, импортирующих command_runner

--- dartpedia/command_runner/lib/command_runner.dart
+++ dartpedia/command_runner/lib/command_runner.dart
@@ -4,5 +4,5 @@ library;
 export 'src/arguments.dart';
 export 'src/command_runner_base.dart';
 export 'src/help_command.dart';
-export 'src/exceptions.dart'; //add
-
+export 'src/exceptions.dart'; 
+export 'src/console.dart';
