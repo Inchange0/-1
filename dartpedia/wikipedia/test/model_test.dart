4e0d97c7dc4d594a85e819977962394d450adfcf
Belova Vera <belova2568@gmail.com>
2026-04-01 21:38:16 +0300

    Исправлены ошибки

--- dartpedia/wikipedia/test/model_test.dart
+++ dartpedia/wikipedia/test/model_test.dart
@@ -1,17 +1,19 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:test/test.dart';
 import 'package:wikipedia/src/model/article.dart';
 import 'package:wikipedia/src/model/search_results.dart';
 import 'package:wikipedia/src/model/summary.dart';
 
-import 'wikipedia_test.dart';
+// Определите константы с путями к файлам
+const String dartLangSummaryJson = './test/test_data/dart_lang_summary.json';
+const String catExtractJson = './test/test_data/cat_extract.json';
+const String openSearchResponse = './test/test_data/open_search_response.json';
 
 void main() {
   group('deserialize example JSON responses from wikipedia API', () {
     test('deserialize Dart Programming Language page summary example data from '
-          'json file into a Summary object', () async {
+        'json file into a Summary object', () async {
       final String pageSummaryInput =
           await File(dartLangSummaryJson).readAsString();
       final Map<String, Object?> pageSummaryMap =
